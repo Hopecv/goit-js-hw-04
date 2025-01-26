64e909326dea6091ca12f43aa8130aab32850d0c
Hopecv <132575400+Hopecv@users.noreply.github.com>
2025-01-26 22:44:24 +0200

    last

--- js/task-3.js
+++ js/task-3.js
@@ -6,8 +6,16 @@ const profile = {
   },
   updatePlayTime(hours) {
     this.playTime += hours;
-  }
+  },
   getInfo() {
     return `${this.Username} has ${this.amount} active hours!`
   }
 };
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
